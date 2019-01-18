3dd191437b7014f7a4222ab8c70834ec1f479d78
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-01-18 19:26:31 +0100

    comment

--- example.cc
+++ example.cc
@@ -15,7 +15,7 @@ main()
 {
   // Make sure we've got the modulation model right.
   using au = adaptive_units<COST>;
-  // Report the length of the longest shortest path.
+  // Set the maximal reach of the spectrally-worse (m = 4) modulation.
   au::longest_reach(8);
   assert(au::units(1, 0) == 1);
   assert(au::units(1, 1) == 1);
