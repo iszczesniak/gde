0ff90a14e9e6e1b130a8321dcd70a5837b7e7e07
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-11-07 22:19:58 +0100

    wip

--- generic_label_creator.hpp
+++ generic_label_creator.hpp
@@ -9,42 +9,37 @@
 #include <optional>
 
 template <typename Label>
-auto
-get_cost(const Label &);
+const auto &
+get_weight(const Label &);
 
 template <typename Label>
-auto
-get_units(const Label &);
+const auto &
+get_resources(const Label &);
 
-template <typename Graph, typename Cost, typename Units>
+template <typename Weight, typename Units>
 class generic_label_creator
 {
-  using Label = generic_label<Graph, Cost, Units>;
+  using Label = generic_label<Weight, Units>;
 
-  // The graph.
-  const Graph &m_g;
-
-  // The number of contiguous units initially requested.
+  // The number of contiguous units initially requested for a demand.
   int m_ncu;
 
   // The optional maximal length.
-  std::optional<COST> m_ml;
+  std::optional<Weight> m_ml;
 
 public:
-  generic_label_creator(const Graph &g, int ncu,
-                        const std::optional<Cost>
-                        &ml = std::optional<Cost>()):
-    m_g(g), m_ncu(ncu), m_ml(ml)
+  generic_label_creator(int ncu,
+                        const std::optional<Weight>
+                        &ml = std::optional<Weight>()):
+    m_ncu(ncu), m_ml(ml)
   {
   }
 
   std::list<Label>
   operator()(const Edge<Graph> &e, const Label &l) const
   {
-    // The cost of the edge.
-    Cost ec = boost::get(boost::edge_weight, m_g, e);
-    // Candidate cost.
-    Cost c_c = get_cost(l) + ec;
+    // Candidate weight.
+    Weight c_c = get_weight(l) + get_weight(e);
 
     // Constriction: consider that path when there is no maximal
     // length given or when the new lenght is not greater than the
@@ -52,19 +47,19 @@ public:
     if (!m_ml || c_c <= m_ml.value())
       {
         // The label units.
-        const Units &l_units = get_units(l);
+        const Units &l_units = get_resources(l);
         // The units available on the edge.
-        const auto &e_su = boost::get(boost::edge_su, m_g, e);
+        const auto &e_su = get_units(e);
         // The candidate SU: the su of label l that can be carried by
         // edge e, and that has at least ncu contiguous units.
         auto c_su = intersection(SU{l_units}, e_su);
-        c_su.remove(adaptive_units<COST>::units(m_ncu, c_c));
+        c_su.remove(adaptive_units<Weight>::units(m_ncu, c_c));
 
 	std::list<Label> l;
 
 	for (auto &cu: c_su)
 	  // The candidate label.
-	  l.push_back(Label(c_c, std::move(cu), e, target(e, m_g)));
+	  l.push_back(Label(c_c, std::move(cu)));
 
 	return l;
       }
