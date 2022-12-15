ae93be3ccdd94bcfdb1b1121c65a6dce786afc20
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-12-15 12:11:27 +0100

    updated the label creator

--- label_creator.hpp
+++ label_creator.hpp
@@ -3,57 +3,48 @@
 
 #include "adaptive_units.hpp"
 #include "generic_label.hpp"
-#include "generic_label_creator.hpp"
 #include "units.hpp"
 
 #include <list>
-#include <optional>
 
-template <typename Weight>
-class label_creator: generic_label_creator
+// Some of the functionality here is similar to generic_label_creator,
+// but it would be hard to use here.  I could call operator() of
+// generic_label_creator, and then remove from the list some labels,
+// but that would be inefficient.  I could pass to operator() a
+// functor that would remove the unwanted slots, but such
+// customization would be too specific, rather unfit to other uses.
+// So it's best just to implement this struct.
+struct label_creator
 {
-  using label_type = generic_label<Weight, Units>;
-
   // The number of contiguous units initially requested for a demand.
   unsigned m_ncu;
 
-public:
-  label_creator(int ncu): m_ncu(ncu)
+  label_creator(unsigned ncu): m_ncu(ncu)
   {
   }
 
   template <typename Label, typename Edge>
-  std::list<label_type>
+  std::list<Label>
   operator()(const Label &l, const Edge &e) const
   {
     // Candidate weight.
-    Weight c_c = get_weight(l) + get_weight(e);
+    auto c_w = get_weight(l) + get_weight(e);
 
-    // Constriction: consider that path when there is no maximal
-    // length given or when the new lenght is not greater than the
-    // limit.
-    if (!m_ml || c_c <= m_ml.value())
-      {
-        // The label units.
-        const Units &l_units = get_resources(l);
-        // The units available on the edge.
-        const auto &e_su = get_resources(e);
-        // The candidate SU: the su of label l that can be carried by
-        // edge e, and that has at least ncu contiguous units.
-        auto c_su = intersection(SU{l_units}, e_su);
-        c_su.remove(adaptive_units<Weight>::units(m_ncu, c_c));
+    // The label units.
+    const auto &l_units = get_resources(l);
+    // The units available on the edge.
+    const auto &e_su = get_resources(e);
+    // The candidate SU: the su of label l that can be carried by
+    // edge e, and that has at least ncu contiguous units.
+    auto c_su = intersection(SU{l_units}, e_su);
+    // Remove the slots that have too few units.
+    c_su.remove(adaptive_units::units(m_ncu, c_w));
 
-	std::list<label_type> l;
+    std::list<Label> l;
 
-	for (auto &cu: c_su)
-	  // The candidate label.
-	  l.push_back(label_type(c_c, std::move(cu)));
-
-	return l;
-      }
-
-    // There is no label.
-    throw true;
+    for (auto &cu: c_su)
+      // The candidate label.
+      l.push_back(label_type(c_w, std::move(cu)));
   }
 };
 
